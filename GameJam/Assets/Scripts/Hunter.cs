adb4c7aff40b3a7b06808b5ab7364bd8412f765c
Nikolaj Nim <nnjo22@student.aau.dk>
2022-10-18 10:45:29 +0200

    Updated player movement

--- GameJam/Assets/Scripts/Hunter.cs
+++ GameJam/Assets/Scripts/Hunter.cs
@@ -9,27 +9,81 @@ public class Hunter : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    private SpriteRenderer flip;
+
     private float moveX;
 
     public float speed = 0;
+
+    public float jumpHeight = 0;
+
+    private bool touchGrass = false;
+
+    private bool flipX;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            touchGrass = true;
+        }
+    }
 
     private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
         moveX = movementVector.x;
+            
+        if (touchGrass && movementVector.y >= 0.6f)
+        {
+            Vector2 jump = new Vector2(0f, jumpHeight);
+            rb.AddForce(jump * jumpHeight);
+            touchGrass = false; 
+        }
+
     }
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        flip = GetComponent<SpriteRenderer>();
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 movement = new Vector2(moveX, 0f);
-        rb.AddForce(movement * speed);
+        if (touchGrass)
+        {
+            if (rb.velocity.x <= speed)
+            {
+                Vector2 movement = new Vector2(moveX, 0f);
+                rb.AddForce(movement * speed);
+            }
+        }
+        else
+        {
+            Vector2 movement = new Vector2(moveX, 0f);
+            rb.AddForce(movement * speed * 0.5f);
+        }
+
+        if(moveX > 0)
+        {
+            flip.flipX = false;
+        }
+        
+        if(moveX < 0) 
+        {
+            flip.flipX = true;
+        }
+
     }
+
+
+
+
+
+
 
    
 }
