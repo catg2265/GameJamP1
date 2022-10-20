08c82453884c765462f1ba4869fd0014c7b988bb
catg2265 <cgrave20@student.aau.dk>
2022-10-20 19:38:50 +0200

    Arrow can hit kudu

--- GameJam/Assets/Scripts/Arrow.cs
+++ GameJam/Assets/Scripts/Arrow.cs
@@ -27,7 +27,7 @@ public class Arrow : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Kudu"))
         {
             Destroy(gameObject);
             rb.isKinematic = true;
