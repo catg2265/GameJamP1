20c16446f987e53af1f0ff0d45f3b4ac85d423dd
Tallest boi of all (Markus) <mflens22@student.aau.dk>
2022-11-29 15:28:35 +0100

    Prototype testable

--- P1ProductForRealThisTime/Assets/Scripts/ActivateEvilEyes.cs
+++ P1ProductForRealThisTime/Assets/Scripts/ActivateEvilEyes.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ActivateEvilEyes : MonoBehaviour
 {
     public float delayAnimation = 2f;
+    public float delayScenechange = 2f;
     public float activateDistance;
     [SerializeField] private Transform player;
     [SerializeField] private Animator anim;
@@ -26,5 +28,7 @@ public class ActivateEvilEyes : MonoBehaviour
     {
         yield return new WaitForSeconds(delayAnimation);
         anim.SetTrigger("Activate");
+        yield return new WaitForSeconds(delayScenechange);
+        SceneManager.LoadScene("Cutscene6");
     }
 }
