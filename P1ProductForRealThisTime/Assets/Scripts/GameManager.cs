01f1ad8894899f9d76dd7e3b4f0996ed1d1bb31f
Tallest boi of all (Markus) <mflens22@student.aau.dk>
2022-11-29 15:18:41 +0100

    Cutscene changes :D 2

--- P1ProductForRealThisTime/Assets/Scripts/GameManager.cs
+++ P1ProductForRealThisTime/Assets/Scripts/GameManager.cs
@@ -15,5 +15,10 @@ public class GameManager : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void startcutscene1()
+    {
+        SceneManager.LoadScene("Cutscene1");
+    }
     #endregion
 }
