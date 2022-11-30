23a4d4fa174168d218753faccdfd83ef15145d71
catg2265 <cgrave20@student.aau.dk>
2022-11-30 18:44:27 +0100

    Everything ready for control/objective screens

--- P1ProductForRealThisTime/Assets/Scripts/GameManager.cs
+++ P1ProductForRealThisTime/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,5 +21,27 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene("Cutscene1");
     }
+    #endregion
+
+    #region  Minigame
+
+    [SerializeField] private GameObject ControlScreen;
+    [SerializeField] private GameObject Minigame;
+    [SerializeField] private GameObject Backgrounds;
+    
+    private void Start()
+    {
+        Minigame.SetActive(false);
+        Backgrounds.SetActive(false);
+        ControlScreen.SetActive(true);
+    }
+
+    public void StartMinigame()
+    {
+        ControlScreen.SetActive(false);
+        Backgrounds.SetActive(true);
+        Minigame.SetActive(true);
+    }
+
     #endregion
 }
