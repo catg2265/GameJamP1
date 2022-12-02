9840396a2b4c51439edd6dca2aab550f264f2e5e
Markus <mflens22@student.aau.dk>
2022-12-02 14:31:36 +0100

    Cutscene 5 text changed

--- P1ProductForRealThisTime/Assets/Scripts/Cutscene5.cs
+++ P1ProductForRealThisTime/Assets/Scripts/Cutscene5.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Cutscene5 : MonoBehaviour
 {
     public int changer = 1;
     public Animator Sceneanimator;
+
+    public int Textchange;
+
+    public TextMeshProUGUI Hymir;
+    public TextMeshProUGUI Jormungand;
+    public TextMeshProUGUI Thor;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Hymir.enabled = true;
+        Jormungand.enabled = false;
+        Thor.enabled = false;
     }
 
     // Update is called once per frame
@@ -19,11 +29,23 @@ public class Cutscene5 : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             Sceneanimator.SetInteger("Cutscenechange", changer++);
-            
+            Textchange++;
         }
         if (Input.GetButton("Jump")&& changer == 4)
         {
             SceneManager.LoadScene("cutscene6");
+        }
+
+        if (Textchange == 1)
+        {
+            Hymir.enabled = false;
+            Jormungand.enabled = true;
+        }
+
+        if (Textchange == 2)
+        {
+            Jormungand.enabled = false;
+            Thor.enabled = true;
         }
     }
 }
