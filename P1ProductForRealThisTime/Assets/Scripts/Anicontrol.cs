7eda9ac096a2b41b4b335b0858033fffcfc29bbd
catg2265 <cgrave20@student.aau.dk>
2022-12-02 14:38:13 +0100

    Final for real this time I promise. Definitely true indeed. yup actually

--- P1ProductForRealThisTime/Assets/Scripts/Anicontrol.cs
+++ P1ProductForRealThisTime/Assets/Scripts/Anicontrol.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Anicontrol : MonoBehaviour
 {
@@ -29,6 +30,9 @@ public class Anicontrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("StartScene");
+        }
     }
 }
