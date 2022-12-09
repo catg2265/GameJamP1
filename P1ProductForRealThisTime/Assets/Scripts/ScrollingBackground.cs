6acb5c00f63843819bfefb1d8931f401874911fd
Lukas Thomsen <lmth22@student.aau.dk>
2022-12-09 10:15:48 +0100

    polished some in gamepart 1 and scrollingbackground script

--- P1ProductForRealThisTime/Assets/Scripts/ScrollingBackground.cs
+++ P1ProductForRealThisTime/Assets/Scripts/ScrollingBackground.cs
@@ -19,25 +19,12 @@ public class ScrollingBackground : MonoBehaviour
       Sprite sprite = GetComponent<SpriteRenderer>().sprite;
       Texture2D texture = sprite.texture;
       textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
-      
    }
 
    private void FixedUpdate()
-   
    {
       Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
       transform.position -= new Vector3(deltaMovement.x * scrollingMultiplier.x, deltaMovement.y * scrollingMultiplier.y);
       lastCameraPosition = cameraTransform.position;
-
-      //enten fix det her (auto generation tingting), eller stretch baggrunden manuelt over den ønskede strækning - laaaangt nemmere dog mindre elegant.
-      //In my opinion så er det fint til at opnå minimal viable product (MVP) og komme videre med livet!
-
-      /*if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
-      {
-         float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
-         transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
-         
-      }
-      */
    }
 }
